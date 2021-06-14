d5b56e29b81b531012039bac955bdd05481a89b9
Filipe Pires <flippires@gmail.com>
2021-06-14 11:53:10 +0100

    add app-level, router-lever and third-party level

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cookieParser = require("cookie-parser");
+
+app.use(logger);
+app.use(cookieParser);
+
+app.get("/", goodbye, (request, response) => {
+  console.log("Home Page");
+  response.send("Welcome to my Middleware App");
+});
+
+app.get("/Users", logger, (request, response) => {
+  console.log("Users Page");
+  response.send("Welcome to your Users Page");
+});
+
+function logger(resquest, response, next) {
+  console.log("Log");
+  next();
+}
+
+function goodbye(request, response, next) {
+  console.log("Adios");
+  next();
+}
 
 app.listen(port, (error) => {
   error ? console.log(error) : console.log(`App runnig at port ${port}`);
 });
+/*
+There is one example with an application-level middleware
+There is one example with an router-level middleware
+There is one example with an third-party middleware 
+*/
